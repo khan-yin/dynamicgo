235199e4fb7847e5d3952ce2c3ac8c3683247e1c
iStitches <1093453695@qq.com>
2023-08-07 12:13:24 +0800

    feat: add prefixError

--- proto/error.go
+++ proto/error.go
@@ -2,6 +2,7 @@ package proto
 
 import (
 	"errors"
+	"fmt"
 	"io"
 )
 
@@ -24,6 +25,14 @@ var (
 	errParse       = errors.New("parse error")
 )
 
+// prefixError add prefix string to specfic error message
+type prefixError struct{ s string }
+
+type wrapError struct {
+	s   string
+	err error
+}
+
 // ParseError converts an error code into an error value.
 // This returns nil if n is a non-negative number.
 func ParseError(n int) error {
@@ -44,4 +53,29 @@ func ParseError(n int) error {
 	default:
 		return errParse
 	}
+}
+
+func (e *prefixError) Error() string {
+	return "proto: " + e.s
+}
+
+func New(f string, x ...interface{}) error {
+	return &prefixError{s: format(f, x...)}
+}
+
+func format(f string, x ...interface{}) string {
+	// avoid "proto: " prefix when chaining
+	for i := 0; i < len(x); i++ {
+		switch e := x[i].(type) {
+		case *prefixError:
+			x[i] = e.s
+		case *wrapError:
+			x[i] = format("%v: %v", e.s, e.err)
+		}
+	}
+	return fmt.Sprintf(f, x...)
+}
+
+func InvalidUTF8(name string) error {
+	return New("field %v contains invalid UTF-8", name)
 }
